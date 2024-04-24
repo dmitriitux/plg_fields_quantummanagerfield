8d776255d9d48ee9f74d182975c7189c0581f53b
dmitriitsymbal <cymbal@delo-design.ru>
2024-04-24 16:38:34 +0300

    правки

--- src/Extension/QuantummanagerField.php
+++ src/Extension/QuantummanagerField.php
@@ -4,7 +4,9 @@ namespace Joomla\Plugin\Fields\QuantumManagerField\Extension;
 
 defined('_JEXEC') or die;
 
+use DOMElement;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
 use Joomla\CMS\Language\Text;
 use Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin;
 
@@ -29,72 +31,86 @@ class QuantummanagerField extends FieldsPlugin
 		// Cache filesystem access / checks
 		static $types_cache = [];
 
-		if ( isset( $types_cache[ $this->_type . $this->_name ] ) )
+		if (isset($types_cache[$this->_type . $this->_name]))
 		{
-			return $types_cache[ $this->_type . $this->_name ];
+			return $types_cache[$this->_type . $this->_name];
 		}
 
 		$types = [];
 
 		// The root of the plugin
-		$root = realpath( JPATH_PLUGINS . '/' . $this->_type . '/' . $this->_name );
+		$root = realpath(JPATH_PLUGINS . '/' . $this->_type . '/' . $this->_name);
 
 		$layout = 'quantummanagerfield';
 
 		// Strip the extension
-		$layout = str_replace( '.php', '', $layout );
+		$layout = str_replace('.php', '', $layout);
 
 		// The data array
 		$data = [];
 
 		// The language key
-		$key = strtoupper( $layout );
+		$key = strtoupper($layout);
 
-		if ( $key != strtoupper( $this->_name ) )
+		if ($key != strtoupper($this->_name))
 		{
-			$key = strtoupper( $this->_name ) . '_' . $layout;
+			$key = strtoupper($this->_name) . '_' . $layout;
 		}
 
 		// Needed attributes
 		$data['type'] = $layout;
 
-		if ( Factory::getLanguage()->hasKey( 'PLG_FIELDS_' . $key . '_LABEL' ) )
+		if (Factory::getLanguage()->hasKey('PLG_FIELDS_' . $key . '_LABEL'))
 		{
-			$data[ 'label' ] = Text::sprintf( 'PLG_FIELDS_' . $key . '_LABEL', strtolower( $key ) );
+			$data['label'] = Text::sprintf('PLG_FIELDS_' . $key . '_LABEL', strtolower($key));
 
 			// Fix wrongly set parentheses in RTL languages
-			if ( Factory::getLanguage()->isRTL() )
+			if (Factory::getLanguage()->isRTL())
 			{
-				$data[ 'label' ] = $data[ 'label' ] . '&#x200E;';
+				$data['label'] = $data['label'] . '&#x200E;';
 			}
 		}
 		else
 		{
-			$data[ 'label' ] = $key;
+			$data['label'] = $key;
 		}
 
 		$path = $root . '/fields';
 
 		// Add the path when it exists
-		if ( file_exists( $path ) )
+		if (file_exists($path))
 		{
-			$data[ 'path' ] = $path;
+			$data['path'] = $path;
 		}
 
 		$path = $root . '/rules';
 
 		// Add the path when it exists
-		if ( file_exists( $path ) )
+		if (file_exists($path))
 		{
-			$data[ 'rules' ] = $path;
+			$data['rules'] = $path;
 		}
 
 		$types[] = $data;
 
 		// Add to cache and return the data
-		$types_cache[ $this->_type . $this->_name ] = $types;
+		$types_cache[$this->_type . $this->_name] = $types;
 
 		return $types;
+	}
+
+	public function onCustomFieldsPrepareDom($field, DOMElement $parent, Form $form)
+	{
+
+		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
+		if (!$fieldNode)
+		{
+			return $fieldNode;
+		}
+
+		$fieldNode->setAttribute('addfieldprefix', 'Joomla\\Plugin\\Fields\\QuantumManagerField\\Field');
+
+		return $fieldNode;
 	}
 
 }
